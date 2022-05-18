2e44714c5da23cd91377628c5c0b58e57fad83ea
Jonathan Reeve <jon.reeve@gmail.com>
2022-05-18 13:50:00 -0400

    already installed this stuff

--- habanero.sh
+++ habanero.sh
@@ -10,12 +10,8 @@
 #SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
 module load cuda11.2/toolkit cuda11.2/blas cudnn8.1-cuda11.2
 module load anaconda
-pip install torch -f https://download.pytorch.org/whl/cpu/torch-1.11.0%2Bcpu-cp38-cp38-linux_x86_64.whl
-pip install torchvision torch-scatter torch-sparse -f https://pytorch-geometric.com/whl/torch-1.11.0+cpu.html
-git clone https://github.com/pytorch/fairseq
 cd fairseq && pip install --editable ./ && cd ..
 python -m spacy download en_core_web_trf
-git clone https://github.com/SapienzaNLP/ewiser.git
 cd ewiser && pip install -r requirements.txt && pip install -e . && cd ..
 python -c "import nltk; nltk.download('wordnet'); nltk.download('omw-1.4')"
 #Command to execute Python program
