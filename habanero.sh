64f35a8c70f1a295e26e6c5daedc40b6d3fff82d
Jonathan Reeve <jon.reeve@gmail.com>
2022-05-18 14:06:00 -0400

    increase memory and cores

--- habanero.sh
+++ habanero.sh
@@ -5,9 +5,9 @@
 #
 #SBATCH --account=free # The account name for the job.
 #SBATCH --job-name=count-objects # The job name.
-#SBATCH -c 8 # The number of cpu cores to use.
+#SBATCH -c 16 # The number of cpu cores to use.
 #SBATCH --time=15:00 # The time the job will take to run.
-#SBATCH --mem-per-cpu=1gb # The memory the job will use per cpu core.
+#SBATCH --mem-per-cpu=2gb # The memory the job will use per cpu core.
 module load cuda11.2/toolkit cuda11.2/blas cudnn8.1-cuda11.2
 module load anaconda
 #Command to execute Python program
