94ceca1a3160c7ab77d917e48c471d0bd333fc72
Jonathan Reeve <jon.reeve@gmail.com>
2022-06-18 14:55:46 -0400

    switch to CUDA

--- habanero.sh
+++ habanero.sh
@@ -6,10 +6,11 @@
 #SBATCH --account=free # The account name for the job.
 #SBATCH --job-name=count-objects # The job name.
 #SBATCH -c 24 # The number of cpu cores to use.
-#SBATCH --time=15:00 # The time the job will take to run.
+#SBATCH --time=0-08:00:00 # The time the job will take to run.
+#SBATCH --constraint=p100 --gres=gpu
 #SBATCH --mem-per-cpu=4gb # The memory the job will use per cpu core.
 module load cuda11.2/toolkit cuda11.2/blas cudnn8.1-cuda11.2
 module load anaconda
 #Command to execute Python program
-python3 countObjects-ewiser.py dubliners.txt
+python3 countObjects-annotate.py cenlab/texts
 #End of script
